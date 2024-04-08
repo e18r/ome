3a56502c0d760673ff3fa53da06dd2971b92e37e
Emilio Silva Schlenker <e18r@disroot.org>
2024-04-07 21:40:11 -0500

    feat(backup): make backup a function

--- backup.sh
+++ backup.sh
@@ -2,33 +2,41 @@
 
 cd "$(dirname $0)"
 
-ENV=test
+backup() {
 
-if [ "${ENV}" = "test" ]; then
-    ENV_UNDER="dev"
-elif [ "${ENV}" = "prod" ]; then
-    ENV_UNDER="test"
-else
-    echo "bad ENV"
-    exit 1
-fi
+    ENV=$1
 
-echo "getting ${ENV} db url..."
-URL=$(./url.sh ${ENV})
+    if [ "${ENV}" = "test" ]; then
+        ENV_UNDER="dev"
+    elif [ "${ENV}" = "prod" ]; then
+        ENV_UNDER="test"
+    else
+        echo "bad ENV"
+        exit 1
+    fi
 
-echo "dumping ${ENV} db..."
-TIMESTAMP=$(date +%s)
-pg_dump --data-only ${URL} > ./dumps/${ENV}/${TIMESTAMP}.sql
-echo "> ./dumps/${ENV}/${TIMESTAMP}.sql"
+    echo "getting ${ENV} db url..."
+    URL=$(./url.sh ${ENV})
 
-echo "uploading ${ENV} db to gcloud..."
-BUCKET=$(jq -r .bucket.${ENV} ./settings.json)
-gcloud storage cp ./dumps/${ENV}/${TIMESTAMP}.sql gs://${BUCKET}
+    echo "dumping ${ENV} db..."
+    TIMESTAMP=$(date +%s)
+    pg_dump --data-only ${URL} > ./dumps/${ENV}/${TIMESTAMP}.sql
+    echo "> ./dumps/${ENV}/${TIMESTAMP}.sql"
 
-echo "deleting ${ENV_UNDER} db..."
-URL_UNDER=$(./url.sh $ENV_UNDER)
-echo "delete from text;" | psql -1 ${URL_UNDER}
-echo "delete from norm;" | psql -1 ${URL_UNDER}
+    echo "uploading ${ENV} db to gcloud..."
+    BUCKET=$(jq -r .bucket.${ENV} ./settings.json)
+    gcloud storage cp ./dumps/${ENV}/${TIMESTAMP}.sql gs://${BUCKET}
 
-echo "restoring ${ENV} db into ${ENV_UNDER} db..."
-psql -1 ${URL_UNDER} < ./dumps/${ENV}/${TIMESTAMP}.sql
+    echo "getting ${ENV_UNDER} url..."
+    URL_UNDER=$(./url.sh $ENV_UNDER)
+
+    echo "deleting ${ENV_UNDER} db..."
+    echo "delete from text;" | psql -1 ${URL_UNDER}
+    echo "delete from norm;" | psql -1 ${URL_UNDER}
+
+    echo "restoring ${ENV} db into ${ENV_UNDER} db..."
+    psql -1 ${URL_UNDER} < ./dumps/${ENV}/${TIMESTAMP}.sql
+
+}
+
+backup test
