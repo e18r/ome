086cd9f05b957c343e05eb8bbf608579bfa1e4f6
Emilio Silva Schlenker <e18r@disroot.org>
2024-11-22 12:14:43 -0500

    devops(url): use different databases for each env

--- url.sh
+++ url.sh
@@ -11,7 +11,8 @@ ENV=$1
 if [ $ENV = "dev" ]; then
     echo "palindr"
 elif [ $ENV = "test" ]; then
-    neon connection-string $ENV
+    PROJECT=$(jq -r .neon.$ENV ./settings.json)
+    neon connection-string --project-id $PROJECT
 else
     PROJECT=$(jq -r .project.$ENV ./settings.json)
     heroku pg:credentials:url -a $PROJECT | grep postgres | xargs
