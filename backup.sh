58c80b77f1e501cbca9e3dda30f94551063d19d5
Emilio Silva Schlenker <e18r@disroot.org>
2024-11-24 22:14:51 -0500

    devops(backup): restore to neon db on same env

--- backup.sh
+++ backup.sh
@@ -27,14 +27,24 @@ backup() {
     BUCKET=$(jq -r .bucket.${ENV} ./settings.json)
     gcloud storage cp ./dumps/${ENV}/${TIMESTAMP}.sql gs://${BUCKET}
 
-    echo "getting heroku ${ENV_UNDER} url..."
+    echo "getting neon ${ENV} db url..."
+    NEON_URL=$(./url.sh ${ENV} neon)
+
+    echo "deleting data from neon ${ENV} db..."
+    echo "delete from text;" | psql -1 ${NEON_URL}
+    echo "delete from norm;" | psql -1 ${NEON_URL}
+
+    echo "heroku ${ENV} -> neon ${ENV}..."
+    psql -1 ${NEON_URL} < ./dumps/${ENV}/${TIMESTAMP}.sql
+
+    echo "getting heroku ${ENV_UNDER} db url..."
     HEROKU_URL_UNDER=$(./url.sh ${ENV_UNDER} heroku)
 
     echo "deleting data from heroku ${ENV_UNDER} db..."
     echo "delete from text;" | psql -1 ${HEROKU_URL_UNDER}
     echo "delete from norm;" | psql -1 ${HEROKU_URL_UNDER}
 
-    echo "restoring data from heroku ${ENV} db into heroku ${ENV_UNDER} db..."
+    echo "heroku ${ENV} -> heroku ${ENV_UNDER}..."
     psql -1 ${HEROKU_URL_UNDER} < ./dumps/${ENV}/${TIMESTAMP}.sql
 
 }
