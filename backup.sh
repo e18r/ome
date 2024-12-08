d4d155f5047dff8c44a4cec9bfa06e278bed5059
Emilio Silva Schlenker <e18r@disroot.org>
2024-12-08 13:46:56 -0500

    devops(backup): make neon primary db

--- backup.sh
+++ backup.sh
@@ -17,42 +17,45 @@ elif [ "${ENV}" = "prod" ]; then
     ENV_UNDER="test"
 fi
 
-echo "getting heroku ${ENV} db url..."
-HEROKU_URL=$(./url.sh ${ENV} heroku)
+echo "getting neon ${ENV} db url..."
+NEON_URL=$(./url.sh ${ENV} neon)
 
-echo "dumping heroku ${ENV} db..."
+echo "dumping neon ${ENV} db..."
 TIMESTAMP=$(date +%s)
-pg_dump --data-only ${HEROKU_URL} > ./dumps/${ENV}/${TIMESTAMP}.sql
+# remove transaction_timeout directive to make it compatible with postgres <v17
+pg_dump --verbose --data-only ${NEON_URL} \
+    | sed "s/SET transaction_timeout.*//g" \
+          > ./dumps/${ENV}/${TIMESTAMP}.sql
 echo "> ./dumps/${ENV}/${TIMESTAMP}.sql"
 
 if [ "${ENV}" != "dev" ]; then
 
-    echo "uploading heroku ${ENV} db to gcloud..."
+    echo "uploading neon ${ENV} db to gcloud..."
     BUCKET=$(jq -r .bucket.${ENV} ./settings.json)
     gcloud storage cp ./dumps/${ENV}/${TIMESTAMP}.sql gs://${BUCKET}
 
 fi
 
-echo "getting neon ${ENV} db url..."
-NEON_URL=$(./url.sh ${ENV} neon)
+echo "getting heroku ${ENV} db url..."
+HEROKU_URL=$(./url.sh ${ENV} heroku)
 
-echo "deleting data from neon ${ENV} db..."
-echo "delete from text;" | psql -1 ${NEON_URL}
-echo "delete from norm;" | psql -1 ${NEON_URL}
+echo "deleting data from heroku ${ENV} db..."
+echo "delete from text;" | psql -1 ${HEROKU_URL}
+echo "delete from norm;" | psql -1 ${HEROKU_URL}
 
-echo "heroku ${ENV} -> neon ${ENV}..."
-psql -1 ${NEON_URL} < ./dumps/${ENV}/${TIMESTAMP}.sql
+echo "neon ${ENV} -> heroku ${ENV}..."
+psql -1 ${HEROKU_URL} < ./dumps/${ENV}/${TIMESTAMP}.sql
 
 if [ "${ENV}" = "dev" ]; then
     exit 0
 fi
 
-echo "getting heroku ${ENV_UNDER} db url..."
-HEROKU_URL_UNDER=$(./url.sh ${ENV_UNDER} heroku)
+echo "getting neon ${ENV_UNDER} db url..."
+NEON_URL_UNDER=$(./url.sh ${ENV_UNDER} neon)
 
-echo "deleting data from heroku ${ENV_UNDER} db..."
-echo "delete from text;" | psql -1 ${HEROKU_URL_UNDER}
-echo "delete from norm;" | psql -1 ${HEROKU_URL_UNDER}
+echo "deleting data from neon ${ENV_UNDER} db..."
+echo "delete from text;" | psql -1 ${NEON_URL_UNDER}
+echo "delete from norm;" | psql -1 ${NEON_URL_UNDER}
 
-echo "heroku ${ENV} -> heroku ${ENV_UNDER}..."
-psql -1 ${HEROKU_URL_UNDER} < ./dumps/${ENV}/${TIMESTAMP}.sql
+echo "neon ${ENV} -> neon ${ENV_UNDER}..."
+psql -1 ${NEON_URL_UNDER} < ./dumps/${ENV}/${TIMESTAMP}.sql
