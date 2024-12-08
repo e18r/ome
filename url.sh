8e84e68478466b4ce7aae780f31f3adaf35a6727
Emilio Silva Schlenker <e18r@disroot.org>
2024-12-08 13:44:06 -0500

    refactor(url): add brackets around variables

--- url.sh
+++ url.sh
@@ -1,9 +1,9 @@
 #! /bin/bash
 
-cd "$(dirname $0)"
+cd "$(dirname ${0})"
 
-if [[ ("$1" != "dev" && "$1" != "test" && "$1" != "prod") \
-   || ("$2" != "heroku" && "$2" != "neon") ]]; then
+if [[ ("${1}" != "dev" && "${1}" != "test" && "${1}" != "prod") \
+   || ("${2}" != "heroku" && "${2}" != "neon") ]]; then
     echo "usage: ./url.sh ENV TYPE"
     echo
     echo "ENV:  dev|test|prod"
@@ -11,14 +11,14 @@ if [[ ("$1" != "dev" && "$1" != "test" && "$1" != "prod") \
     exit 1
 fi
 
-ENV=$1
-TYPE=$2
-if [ $ENV = "dev" ]; then
+ENV=${1}
+TYPE=${2}
+if [ ${ENV} = "dev" ]; then
     jq -r .${TYPE}.dev ./settings.json
-elif [ $TYPE = "neon" ]; then
-    PROJECT=$(jq -r .neon.$ENV ./settings.json)
-    neon connection-string --project-id $PROJECT
+elif [ ${TYPE} = "neon" ]; then
+    PROJECT=$(jq -r .neon.${ENV} ./settings.json)
+    neon connection-string --project-id ${PROJECT}
 else
-    PROJECT=$(jq -r .heroku.$ENV ./settings.json)
-    heroku pg:credentials:url -a $PROJECT | grep postgres | xargs
+    PROJECT=$(jq -r .heroku.${ENV} ./settings.json)
+    heroku pg:credentials:url -a ${PROJECT} | grep postgres | xargs
 fi
